ece16ca9866a413076133cc4b6dec711c278e00c
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2025-03-19 03:10:39 +0500

    added the http:// in railways urls for backend apis

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENVIRONMENT === "production"
-    ? "/online-store-production-16af.up.railway.app"
-    : "/online-store-production-16af.up.railway.app";
+    ? "https://online-store-production-16af.up.railway.app"
+    : "https://online-store-production-16af.up.railway.app";
 
 const axiosInstance = axios.create({
   baseURL,

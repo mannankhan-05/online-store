1636f50aeac3089998c7c647101468073e9c8725
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2025-03-19 01:21:21 +0500

    added the railway databse credentials

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -6,7 +6,8 @@ const db = new Sequelize(
   process.env.DATABASE_USER || "default",
   process.env.DATABASE_PASSWORD || "default",
   {
-    host: "localhost", // that part was generating error on the recipe-costManagement application
+    host: process.env.DATABASE_HOST, // that part was generating error on the recipe-costManagement application
+    port: Number(process.env.DATABASE_PORT) || 5432, // Explicitly define the port
     dialect: "postgres",
   }
 );

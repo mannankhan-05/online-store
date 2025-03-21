bda7939cc0e1e60e1ff86b74a09d8d26f92ba071
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2025-03-22 02:26:37 +0500

    added logs to see why production database is not sending apis to railway backend

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENVIRONMENT === "production"
     ? "https://online-store-production-16af.up.railway.app"
-    : "http://localhost:4000";
+    : // : "http://localhost:4000";
+      "https://online-store-production-16af.up.railway.app";
 
 const axiosInstance = axios.create({
   baseURL,

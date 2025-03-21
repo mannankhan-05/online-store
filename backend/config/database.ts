7bc40b9c9e045c2b2de049fe3d6e19b3f55da201
Abdul Mannan Khan <abdulmannan.khan005@gmail.com>
2025-03-22 02:34:55 +0500

    solved the problem : added the DATABASE_ENVIRONMENT variable to netlify

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -6,16 +6,6 @@ const envFile =
   process.env.NODE_ENV === "production" ? ".env.production" : ".env.local";
 dotenv.config({ path: envFile });
 
-console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("DB_HOST:", process.env.DATABASE_HOST);
-console.log("DB_USER:", process.env.DATABASE_USER);
-console.log("DB_NAME:", process.env.DATABASE_NAME);
-console.log(
-  "DB_PASSWORD:",
-  process.env.DATABASE_PASSWORD ? "*****" : "Not set"
-);
-console.log("PORT:", process.env.PORT);
-
 const db = new Sequelize(
   process.env.DATABASE_NAME || "default",
   process.env.DATABASE_USER || "default",
--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENVIRONMENT === "production"
     ? "https://online-store-production-16af.up.railway.app"
-    : // : "http://localhost:4000";
-      "https://online-store-production-16af.up.railway.app";
+    : "http://localhost:4000";
 
 const axiosInstance = axios.create({
   baseURL,
